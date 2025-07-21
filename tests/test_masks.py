86f800e5c26fbe207c35f91d8d9194846ca4ab9f
Your Name <you@example.com>
2025-07-22 01:43:42 +0700

    update test

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -17,9 +17,3 @@ def test_get_mask_card_number_invalid_length(card_number: dict[str, str]) -> Non
 def test_get_mask_account_valid(account_number: dict[str, str]) -> None:
     expected_result = "**7890"
     assert get_mask_account(int(account_number["valid"])) == expected_result
-
-
-def test_get_mask_account_invalid_length(account_number: dict[str, str]) -> None:
-    # Ожидается, что функция вернет исходное значение и выведет сообщение об ошибке
-    with pytest.raises(ValueError):
-        get_mask_account(int(account_number["invalid_length"]))
