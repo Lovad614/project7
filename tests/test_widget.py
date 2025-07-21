7a708c9217f1b4622a698a4938c97e79cb8a45f0
Your Name <you@example.com>
2025-07-22 02:30:14 +0700

    add docstring

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,9 +2,19 @@ from src.widget import get_date, mask_account_card
 
 
 def test_mask_account_card(card_number: dict[str, str], account_number: dict[str, str]) -> None:
+    """
+    тестирование функции mask_account_card.
+    :param card_number: номер карты по ключу valid
+    :param account_number: номер счета по ключу valid
+    """
+
     assert mask_account_card(f"Счет {account_number['valid']}") == "Счет **7890"
     assert mask_account_card(f"Карта VISA {card_number['valid']}") == "Карта VISA 1234 56** **** 5678"
 
 
 def test_get_date(date_number: dict[str, str]) -> None:
+    """
+    Тестирование get_date.
+    :param date_number: номер даты по ключу valid
+    """
     assert get_date(date_number["valid"]) == "05.10.2023"
